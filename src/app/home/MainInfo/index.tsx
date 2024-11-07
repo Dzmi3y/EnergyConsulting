704ac704fcdbb01db34164b95db2a0fc707be3ef
Dzmitry <dzmi3y@gmail.com>
2024-11-08 00:56:11 +0300

    responsive design added to home page MainInfo block

--- src/app/home/MainInfo/index.tsx
+++ src/app/home/MainInfo/index.tsx
@@ -5,25 +5,35 @@ import ViewPortfolioButton from "@/components/Buttons/ViewPortfolioButton";
 
 const MainInfo = () => {
   return (
-    <div className={`flex justify-between ${styles.container}`}>
-      <div className="flex flex-col gap-10 mb-16 justify-end global-left-margins">
-        <h1 className="text-6xl">
+    <div className={styles.container}>
+      <div className={`${styles["info-container"]} global-left-margins`}>
+        <h1 className={styles.title}>
           Redefining <br /> Energy
         </h1>
-        <div className="text-2xl max-w-screen-md">
+        <div className={styles.description}>
           As market leaders for 20 years, we are experts in energy efficiency,
           providing tailored, concrete solutions to meet your needs.
         </div>
         <ViewPortfolioButton text="View our Portfolio" />
       </div>
-      <Image
-        aria-hidden
-        className="h-screen"
-        src="/images/home/flashlight.png"
-        alt="Flashlight image"
-        width="756"
-        height="982"
-      />
+      <div className="max-h-[507px] overflow-hidden lg:max-h-none">
+        <Image
+          aria-hidden
+          className="w-screen  object-cover h-[507px] lg:h-auto lg:max-w-xl xl:max-w-xl 2xl:max-w-2xl hidden md:block"
+          src="/images/home/flashlight.png"
+          alt="Flashlight image"
+          width="756"
+          height="982"
+        />
+        <Image
+          aria-hidden
+          className="w-screen object-cover h-[507px] display md:hidden"
+          src="/images/home/flashlight_mobile.png"
+          alt="Flashlight image"
+          width="390"
+          height="507"
+        />
+      </div>
     </div>
   );
 };
